9376d02c86afb0bb49f484019d2980a9829eed02
Nicholas Frechette <zeno490@gmail.com>
2018-07-16 21:15:22 -0400

    Adding log cleaning script when profiling decompression.

---
+++ Tools/clean_log.py
@@ -0,0 +1,33 @@
+import os
+import re
+import sys
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print('Invalid usage: python {}'.format(' '.join(sys.argv)))
+		print('Usage: python clean_log.py <path/to/log/file.txt>')
+		sys.exit(1)
+
+	log_filename = sys.argv[1]
+	with open(log_filename, 'r') as file:
+		file_content = file.read()
+		matches = re.findall('^LogPlayLevel: UE4Game: LogAnimation:Warning: \[\[(.+)\]\].*', file_content, re.MULTILINE)
+		if len(matches) == 0:
+			matches = re.findall('^\[.+\]\[.+\]LogAnimation:Warning: \[\[(.+)\]\].*', file_content, re.MULTILINE)
+		if len(matches) == 0:
+			matches = re.findall('^\[.+\]\[.+\]LogPlayLevel: \[.+\]\[.+\]LogAnimation: Warning: \[\[(.+)\]\].*', file_content, re.MULTILINE)
+		if len(matches) == 0:
+			matches = re.findall('^\[.+\]\[.+\]LogAnimation: Warning: \[\[(.+)\]\].*', file_content, re.MULTILINE)
+
+		print('Found {} CSV lines'.format(len(matches)))
+		if len(matches) > 0:
+			csv_filename = os.path.splitext(log_filename)[0] + '.csv'
+			with open(csv_filename, 'w') as csv_file:
+				print('Long Clip Name,Sample Time, Decompression Time (ms), Codec, Decompression Time (us), Clip Name', file = csv_file)
+				for line in matches:
+					values = line.split(',')
+					duration_us = float(values[2]) * 1000.0
+					short_name = values[0].split('.')[1]
+					print('{},{},{}'.format(line, duration_us, short_name), file = csv_file)
+
