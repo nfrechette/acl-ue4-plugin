26cf88d56b766a0f693e0d11a42e290ffa84d5f1
Nicholas Frechette <zeno490@gmail.com>
2019-06-01 19:22:21 -0400

    perf(arm): enable popcount intrinsics

--- ACLPlugin/Source/ACLPlugin/Public/ACLImpl.h
+++ ACLPlugin/Source/ACLPlugin/Public/ACLImpl.h
@@ -36,7 +36,7 @@
 	#define ACL_ASSERT(expression, format, ...) checkf(expression, TEXT(format), #__VA_ARGS__)
 #endif
 
-#if PLATFORM_ENABLE_POPCNT_INTRINSIC || PLATFORM_XBOXONE || PLATFORM_PS4
+#if PLATFORM_ENABLE_POPCNT_INTRINSIC || PLATFORM_XBOXONE || PLATFORM_PS4 || PLATFORM_ENABLE_VECTORINTRINSICS_NEON
 	// Enable usage of popcount instruction if present and requested
 	#define ACL_USE_POPCOUNT
 #endif
