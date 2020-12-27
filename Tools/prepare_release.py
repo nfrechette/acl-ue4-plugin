20761780b1a2c90dc5cb1c1f495bbf4859f5f1b7
Nicholas Frechette <zeno490@gmail.com>
2020-12-27 12:22:10 -0500

    chore: add UE4 version to release preparation script
    Also clean up a bit how we remove dependencies we don't need.

--- Tools/prepare_release.py
+++ Tools/prepare_release.py
@@ -3,6 +3,13 @@ import shutil
 import sys
 
 if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print('Invalid usage: python {}'.format(' '.join(sys.argv)))
+		print('Usage: python prepare_release.py <version> (e.g. 4.25)')
+		sys.exit(1)
+
+	target_ue4_version = sys.argv[1]
+
 	tools_dir = os.path.dirname(os.path.realpath(__file__))
 	root_dir = os.path.join(tools_dir, '..')
 	staging_dir = os.path.join(root_dir, 'Staging')
@@ -22,21 +29,26 @@ if __name__ == "__main__":
 	# Remove catch2 and other third party dependencies we do not own or need
 	print('Removing what we don\'t need ...')
 	acl_root_dir = os.path.join(plugin_dst_dir, 'Source', 'ThirdParty', 'acl')
-	shutil.rmtree(os.path.join(acl_root_dir, 'external', 'catch2'))
-
 	rtm_root_dir = os.path.join(acl_root_dir, 'external', 'rtm')
-	shutil.rmtree(os.path.join(rtm_root_dir, 'external', 'catch2'))
-
-	rtm_benchmark_dir = os.path.join(rtm_root_dir, 'external', 'benchmark')
-	if os.path.exists(rtm_benchmark_dir):
-		print('NEED TO REMOVE RTM BENCHMARK')	# Will be added in RTM 2.0, make sure we don't forget it
-		sys.exit(1)
-
 	sjsoncpp_root_dir = os.path.join(acl_root_dir, 'external', 'sjson-cpp')
+
+	shutil.rmtree(os.path.join(acl_root_dir, 'external', 'catch2'))
+	shutil.rmtree(os.path.join(rtm_root_dir, 'external', 'catch2'))
 	shutil.rmtree(os.path.join(sjsoncpp_root_dir, 'external', 'catch2'))
 
+	shutil.rmtree(os.path.join(acl_root_dir, 'external', 'benchmark'))
+	shutil.rmtree(os.path.join(rtm_root_dir, 'external', 'benchmark'))
+
+	print('Setting uplugin version to: {} ...'.format(target_ue4_version))
+	uplugin_file = os.path.join(plugin_dst_dir, 'ACLPlugin.uplugin')
+	with open(uplugin_file) as f:
+		uplugin_file_content = f.read()
+	uplugin_file_content = uplugin_file_content.replace('4.25.0', target_ue4_version + '.0')
+	with open(uplugin_file, 'w') as f:
+		f.write(uplugin_file_content)
+
 	print('Zipping ACLPlugin ...')
-	zip_filename = os.path.join(root_dir, 'ACLPlugin')
+	zip_filename = os.path.join(root_dir, 'ACLPlugin_' + target_ue4_version)
 	shutil.make_archive(zip_filename, 'zip', staging_dir)
 
 	print('Done!')
