79eec4e5d4c11a060bf4f67791953ade114bc032
Nicholas Frechette <zeno490@gmail.com>
2023-12-04 21:24:12 -0500

    fix: improve CSV tally and clean up columns

--- ACLPlugin/Extras/tally_anim_csv.py
+++ ACLPlugin/Extras/tally_anim_csv.py
@@ -21,13 +21,30 @@ if __name__ == "__main__":
 		print('No CSV data found!')
 		sys.exit(1)
 
-	header_row = rows[0]
+	header_row = rows[-2]
+	if rows[-1][0] != '[HasHeaderRowAtEnd]':
+		print('Expected the header row at the end')
+		sys.exit(1)
+
 	if header_row[-1] == 'Animation/Total/ExtractPoseFromAnimData':
 		print('This CSV file has already been processed!')
 		sys.exit(1)
 
+	num_columns = len(header_row)
+	num_rows = len(rows)
+
+	# The header is written last because new categories are added as they are found
+	# As such, some rows can have missing columns
+	# We pad them all with zeroes
+	for row_idx in range(1, num_rows - 2):
+		row = rows[row_idx]
+
+		num_missing_columns = num_columns - len(row)
+		if num_missing_columns > 0:
+			row.extend(["0"] * num_missing_columns)
+
 	# Process our rows
-	# First append our new header to the first row as the last column
+	# First append our new header to the header row as the last column
 
 	num_columns = len(header_row)
 	num_rows = len(rows)
@@ -53,8 +70,8 @@ if __name__ == "__main__":
 		# By default, everything is in milliseconds
 		row.append(str(total_decomp_time))
 
-	# The second to last row must match our header
-	rows[-2].append('Animation/Total/ExtractPoseFromAnimData')
+	# Make sure both header rows match
+	rows[0] = header_row
 
 	# Write out our modified rows over the same file
 	with open(csv_filename, 'w', newline='') as csv_file:
